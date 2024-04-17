f1db6a0f0ed961f2ea9ea843e9a4a70054ee00e2
Yabiti <yabetseyohannes@gmail.com>
2024-04-17 12:41:51 +0300

    .

--- yab.py
+++ yab.py
@@ -1,4 +1,13 @@
 from pyfiglet import Figlet
+import sys
 
 figlet = Figlet()
 figlet.getFonts()
+
+if len(sys.argv) == 0:
+    print(figlet.renderText(s))
+elif len(sys.argv) == 2:
+    print(figlet.renderText(s))
+
+else:
+    sys.exit("Error")
